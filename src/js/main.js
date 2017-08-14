106975dafd411824ce3303ee811e293a86905378
VIRIATO <javi.lindo@gmail.com>
2017-08-14 19:02:37 +0200

    Se añade más elementos responsive, un botón Go to Top y section de otras noticias

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 window.$ = window.jQuery =  require("jquery");
+
+
+//Funcionalidad para el botón de subir al top de la página
+function subir() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
+$( document ).ready(function() {
+
+    var backTop = document.getElementById("back-to-top");
+    backTop.addEventListener('click', subir);
+});
+
+
